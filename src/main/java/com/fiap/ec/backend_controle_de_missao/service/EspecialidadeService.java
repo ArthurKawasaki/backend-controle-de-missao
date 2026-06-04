01221c07c253b8da53d3c0801eaa356b849ec608
ArthurKawasaki <arthurkawasaki.akk2@.com>
2026-06-03 22:27:14 -0300

    feat: criação da camada service de Especialidade

---
+++ src/main/java/com/fiap/ec/backend_controle_de_missao/service/EspecialidadeService.java
@@ -0,0 +1,20 @@
+package com.fiap.ec.backend_controle_de_missao.service;
+
+package com.fiap.ec.backend_controle_de_missao.service;
+import com.fiap.ec.backend_controle_de_missao.model.Especialidade;
+import com.fiap.ec.backend_controle_de_missao.repository.EspecialidadeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class EspecialidadeService {
+    private final EspecialidadeRepository repository;
+    public EspecialidadeService(EspecialidadeRepository repository) {
+        this.repository = repository;
+    }
+    public Especialidade salvar(Especialidade especialidade) {
+        return repository.save(especialidade);
+    }
+    public List<Especialidade> listar() {
+        return repository.findAll();
+    }
+}
