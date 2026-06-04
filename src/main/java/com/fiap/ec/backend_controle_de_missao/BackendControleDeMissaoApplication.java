772b35308a04b819867d1cd805904ddadbed91b6
ArthurKawasaki <arthurkawasaki.akk2@.com>
2026-06-03 22:12:17 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/com/fiap/ec/backend_controle_de_missao/BackendControleDeMissaoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_controle_de_missao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendControleDeMissaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendControleDeMissaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ec/backend_controle_de_missao/BackendControleDeMissaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ec.backend_controle_de_missao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendControleDeMissaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
